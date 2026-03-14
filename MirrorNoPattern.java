28356f27d7fbffca3992f125e887cb2ea6851aab
rathodleena <102647873+rathodleena@users.noreply.github.com>
2026-03-14 12:50:12 +0530

    Update MirrorNoPattern.java solution

--- MirrorNoPattern.java
+++ MirrorNoPattern.java
@@ -2,6 +2,10 @@ public class MirrorNoPattern {
   public static void main(String[] args) {
     int n =5;
     for(int i = 1; i <= n; i++) {
+      //print space
+      for(int k = 1; k <= n-i; k++) {
+        System.out.print(" ");
+      }
       for(int j = 1; j <= i; j++) {
         System.out.print(j);
       }
