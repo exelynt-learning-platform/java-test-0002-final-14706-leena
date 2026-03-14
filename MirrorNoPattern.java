70ff98bb991cfd107400758a17fa1565f4445233
rathodleena <102647873+rathodleena@users.noreply.github.com>
2026-03-14 13:03:55 +0530

    Update MirrorNoPattern.java Code
    Code done

--- MirrorNoPattern.java
+++ MirrorNoPattern.java
@@ -1,18 +1,24 @@
 public class MirrorNoPattern {
-  public static void main(String[] args) {
-    int n =5;
-    for(int i = 1; i <= n; i++) {
-      //print space
-      for(int k = 1; k <= n-i; k++) {
-        System.out.print("  ");
-      }
-      for(int j = 1; j <= i; j++) {
-        System.out.print(j);
-      }
-      for(int j = i-1; j >= 1; j--) {
-        System.out.print(j);
-      }
-      System.out.println();
+
+    public static void main(String[] args) {
+
+        int rows = 5;
+
+        for (int i = 1; i <= rows; i++) {
+
+            for (int space = 1; space <= rows - i; space++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
     }
-  }
 }
